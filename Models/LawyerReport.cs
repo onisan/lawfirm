44daadee275acd491037200a62241d377d8e162e
phunkeybase <ncr.soul@gmail.com>
2013-11-23 16:44:58 -0800

    basic Lawyer Report working, detail view next

---
+++ Models/LawyerReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLawFirm.Models
+{
+    public class LawyerReport : NRBM_LAWYER
+    {
+        public decimal Earnings { get; set; }
+        public decimal Expenses { get; set; }
+        public int Appointments { get; set; }
+        public int CourtAppearances { get; set; }
+        public int Cases { get; set; }
+        public int Clients { get; set; }
+    }
+}
